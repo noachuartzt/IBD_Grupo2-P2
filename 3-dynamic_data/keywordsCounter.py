68c4cca5d0abdc179b865f8c2d32f92b385c2739
Alejandro Pequeño L <pq.lz.alejandro@gmail.com>
2023-05-15 15:06:47 +0200

    spark del apaertado 3 probado y mejorado para admitir col = null

--- 3-dynamic_data/keywordsCounter.py
+++ 3-dynamic_data/keywordsCounter.py
@@ -1,23 +1,33 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import explode, split
+from pyspark.sql.types import *
+from pyspark.sql.functions import col, isnull
 
-# Crear la sesión de Spark
+# Create the Spark session
 spark = SparkSession.builder.appName("keywordCount").getOrCreate()
 
-# Leer el archivo JSON a un DataFrame
-json_df = spark.read.json("path/to/json/file.json")
+# Read the JSON file into a DataFrame
+json_df = spark.read.json("datain/2ca14fe14f0bd2f1363f3b735e788d12c3f9f332.json")
 
-# Seleccionar el campo text y convertirlo en un RDD
-words_rdd = json_df.select("text").rdd.flatMap(lambda x: x[0].split())
+# Add a new column 'is_null' indicating whether 'abstract' column is null or not
+json_df_with_null = json_df.withColumn('is_null', col('abstract').isNull())
 
-# Lista de palabras
-words_to_count = ["a", "b", "c", "d", "e]
+# Show the 'is_null' column
+if json_df_with_null.select('is_null').first()[0] == False:
+    
+    # Seleccionar el campo text y convertirlo en un RDD
+    words_rdd = json_df.select("abstract").rdd.flatMap(lambda x: x[0].split())
 
-# Contar las palabras
-counts_rdd = words_rdd.filter(lambda word: word in words_to_count).map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
+    # Lista de palabras
+    words_to_count = ["science", "artificial", "intelligence", "the", "The"]
 
-# Mostrar los resultados
-print(counts_rdd.collect())
+    # Contar las palabras
+    counts_rdd = words_rdd.filter(lambda word: word in words_to_count).map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
+
+    # Mostrar los resultados
+    print(counts_rdd.collect())
+    
+else:
+    raise ValueError("The 'abstract' section of the paper contains null values. Please select another section of the paper or instead another paper.")
 
 # Cerrar la sesión de Spark
 spark.stop()
--- 5-complex_queries/words_queries/wordLengthCounter.py
+++ 5-complex_queries/words_queries/wordLengthCounter.py
@@ -1,16 +1,21 @@
-from pyspark import SparkConf, SparkContext
-import json
+from pyspark.sql import SparkSession
+from pyspark.sql.types import *
+from pyspark.sql.functions import col, isnull
+import json_rdd
 
 spark = SparkSession.builder.appName("keywordCount").getOrCreate()
 
 # Leer el archivo JSON a un DataFrame
-json_df = spark.read.json("path/to/json/file.json")
+json_files = ["2ca14fe14f0bd2f1363f3b735e788d12c3f9f332.json", "7dee9f8f534df0cbb38b12d3bb7c84f86c704fd0.json"]
+json_df = spark.read.json(f"datain/{json_files[0]}")
+
+# JUNTAR LOS JSON_DF EN UN SOLO ?
 
 # Analizar cada registro JSON
 records_rdd = json_rdd.map(json.loads)
 
 # Seleccionar el campo text y convertirlo en un RDD
-words_rdd = json_df.select("text").rdd.flatMap(lambda x: x[0].split())
+words_rdd = json_df.select("abstract").rdd.flatMap(lambda x: x[0].split())
 
 # Longitud de las palabras a contar
 n = 5
