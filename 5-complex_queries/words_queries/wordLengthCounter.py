51bb83eba43d2502c7f05c88a87f60c0e657553b
Alejandro PL <90752364+AlejandroPqLz@users.noreply.github.com>
2023-05-16 20:54:00 +0200

    Update wordLengthCounter.py

--- 5-complex_queries/words_queries/wordLengthCounter.py
+++ 5-complex_queries/words_queries/wordLengthCounter.py
@@ -3,20 +3,24 @@ from pyspark.sql.types import *
 from pyspark.sql.functions import col, isnull
 
 # Create the Spark session
-spark = SparkSession.builder.\
+spark = SparkSession.\
+        builder.\
         appName("lengthwordCount").\
         master("spark://spark-master:7077").\
-        config("spark.executor.memory", "10g").\
+        config("spark.executor.memory", "5g").\
         config("spark.eventLog.enabled", "true").\
-        config("spark.eventLog.dir", "file://opt/workspace/events").\
+        config("spark.eventLog.dir", "file:///opt/workspace/events").\
         getOrCreate()
 
 # Preprocesado
 # =====================================================================
 # Leer todos los archivos JSON del corpus (uniéndolos en un mismo DataFrame)
-corpus = ["2ca14fe14f0bd2f1363f3b735e788d12c3f9f332.json","7dee9f8f534df0cbb38b12d3bb7c84f86c704fd0.json",\
-          "9e2501b8e5da6e1627508c4eb321bf66eb41020e.json","80313ba1f12e4525b941ba29f8e020cf5ae8b835.json",\
-          "2038383fedccdf0b8c1efcb0832ecd18b481b3c1.json","f8d9409606abc438537d3a249b56ec0ac8e62e91.json"]
+corpus = ["2ca14fe14f0bd2f1363f3b735e788d12c3f9f332.json","7dee9f8f534df0cbb38b12d3bb7c84f86c704fd0.json"]
+
+# corpus = ["2ca14fe14f0bd2f1363f3b735e788d12c3f9f332.json","7dee9f8f534df0cbb38b12d3bb7c84f86c704fd0.json",\
+#           "9e2501b8e5da6e1627508c4eb321bf66eb41020e.json","80313ba1f12e4525b941ba29f8e020cf5ae8b835.json",\
+#           "2038383fedccdf0b8c1efcb0832ecd18b481b3c1.json","f8d9409606abc438537d3a249b56ec0ac8e62e91.json"]
+
 json_df = spark.read.json(["datain/" + file for file in corpus])
 
 # Eliminar registros con abstract nulo
