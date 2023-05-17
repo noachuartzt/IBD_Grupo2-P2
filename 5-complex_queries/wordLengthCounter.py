97e67ed2b1bb81ea9219a44a0112fe8fb603ffe7
carlota-upm <carlota.medrano@alumnos.upm.es>
2023-05-17 10:38:44 +0200

    mp2

--- 5-complex_queries/wordLengthCounter.py
+++ 5-complex_queries/wordLengthCounter.py
@@ -1,12 +1,12 @@
-import shutil
-import glob
-import csv
+from pyspark.sql import SparkSession
+from pyspark.sql.types import *
+from pyspark.sql.functions import col, isnull
 import os
 
 # Crear la sesión Spark
 spark = SparkSession.\
         builder.\
-        appName("keywordCount").\
+        appName("lengthwordCount").\
         master("spark://spark-master:7077").\
         config("spark.executor.memory", "5g").\
         config("spark.eventLog.enabled", "true").\
@@ -16,7 +16,7 @@ spark = SparkSession.\
 # Preprocesado
 # =====================================================================
 # Leer todos los archivos JSON del corpus (uniéndolos en un mismo DataFrame)
-directory = './datain/'
+directory = './datain'
 
 # Lista de archivos en el directorio
 corpus = os.listdir(directory)
@@ -31,40 +31,15 @@ json_df_filtered = json_df.filter(col('abstract').isNotNull())
 # Seleccionar el campo abstract y convertirlo en un RDD
 words_rdd = json_df_filtered.select("abstract").rdd.flatMap(lambda x: x[0].split())
 
-# Lista de palabras
-words_to_count = ["Science", "artificial", "intelligence", "The"]
-words_to_count = [word.lower() for word in words_to_count]
+# Longitud de las palabras a contar
+n = 6
 
-# Contar las palabras
-counts_rdd = words_rdd.filter(lambda word: word.lower() in words_to_count).map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
+# Filtrar palabras con longitud n y contar su frecuencia
+counts_rdd = words_rdd.filter(lambda word: len(word) == n).map(lambda word: (n, 1)).reduceByKey(lambda a, b: a + b)
+# counts_rdd = words_rdd.filter(lambda word: len(word) == n).map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b) # si se quieren saber cuales son las palabras de longitud n
 
-# Mostrar los resultados
+# Mostrar resultados
 print(counts_rdd.collect())
-
-# Keywords.csv
-# =====================================================================
-# Guardar en CSV
-counts_df = counts_rdd.toDF() # convertir a DataFrame la lista resultado
-counts_df.write.csv("dataout/temp", mode="overwrite") # guardamos los resultados de cada worker
-
-# Obtener todos los archivos CSV generados en el directorio temporal
-csv_files = glob.glob("dataout/temp/*.csv")
-
-# Fusionar los archivos CSV en uno solo
-with open("dataout/Keywords.csv", "w", newline="") as outfile:
-    writer = csv.writer(outfile)
-    writer.writerow(["word", "frequency"]) # Escribir los nuevos nombres de las columnas
-
-    # Leer cada archivo CSV y copiar sus contenidos al archivo de salida
-    for csv_file in csv_files:
-        with open(csv_file, "r", newline="") as infile:
-            reader = csv.reader(infile)
-            for row in reader:
-                writer.writerow(row)
-                
-# Borramos el fichero temp
-folder_path = "dataout/temp"
-shutil.rmtree(folder_path) # borramos la carpeta y su contenido
 
 # Cerrar la sesión de Spark
 spark.stop()
